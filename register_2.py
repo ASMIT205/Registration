2860edd8ff6fac10f0d91c97957d27c380d59d65
ASMIT205 <78749674+ASMIT205@users.noreply.github.com>
2024-04-24 03:20:20 +0530

    Update register_2.py

--- register_2.py
+++ register_2.py
@@ -4,7 +4,10 @@ from flask_cors import CORS
 from datetime import datetime
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import UniqueConstraint
-import re 
+import re
+import qrcode
+from google.cloud import storage 
+import json
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -24,16 +27,56 @@ def calculate_age(birth_date):
     
     return age   
 
+#def QR_GENERATOR(doctor_id):
+    #myqr = qrcode.make(doctor_id)
+    #myqr.save(f'QR/{doctor_id}.png')
+    #unique_file_path_name = f'QR/{doctor_id}.png'
+    #upload_to_bucket(unique_file_path_name,unique_file_path_name,'extraction_medi')
+
+
+
+def QR_GENERATOR(doctor_id,full_name,name_of_hospital):
+    data ={
+            "doctor_id":doctor_id,
+            "full_name":full_name,
+            "name_of_hospital": name_of_hospital
+    }
+    json_data = json.dumps(data)
+    #myqr = qrcode.make(doctor_id)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(json_data)
+    qr.make(fit=True)
+    #qr.save(f'QR/{doctor_id}.png')
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the QR code image
+    unique_file_path_name = f'QR/{doctor_id}.png'
+    img.save(unique_file_path_name)
+    
+    upload_to_bucket(unique_file_path_name,unique_file_path_name,'extraction_medi')
+
+storage_client = storage.Client.from_service_account_json('project1-419908-856d7a3f35c7.json')
+
+my_bucket = storage_client.get_bucket('extraction_medi')
+
+def upload_to_bucket(blob_name,file_path,bucket_name):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(file_path)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+
 class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    patients = db.relationship('UserDetails', secondary='doctor_patient_association', backref=db.backref('doctors', lazy='dynamic'))
-
-class NewTable(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    
-class DoctorDetails(db.Model):
-    id = db.Column(db.Integer, primary_key =True)
     user_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
@@ -45,14 +88,39 @@ class DoctorDetails(db.Model):
     year_of_passing = db.Column(db.Integer, nullable=False)
 
     gender = db.Column(db.String(10),nullable=False)
+    name_of_hospital = db.Column(db.String(100),nullable=False )
     Designation = db.Column(db.String(10),nullable=False)
     medical_council_member = db.Column(db.Integer,nullable=False)
     certificate = db.Column(db.String(255),nullable=False)
     speciality = db.Column(db.String(10),nullable=False)
     About_the_doctor = db.Column(db.String(255))
+    patients = db.relationship('UserDetails', secondary='doctor_patient_association', backref=db.backref('doctors', lazy='dynamic'))
 
-    # Define a relationship to the Doctor Table
-    doctor = db.relationship('Doctor',backref= db.backref('doctor_details',lazy=True))
+# class NewTable(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+    
+# class DoctorDetails(db.Model):
+#     id = db.Column(db.Integer, primary_key =True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
+#     phone_number = db.Column(db.Integer, unique=True, nullable=False)
+#     first_name = db.Column(db.String(100), nullable=False)
+#     last_name = db.Column(db.String(100), nullable=False) 
+#     date_of_birth = db.Column(db.Date,nullable=False) 
+#     age = db.Column(db.Integer, nullable=False) 
+#     #personal _qualification
+#     First_qualification = db.Column(db.String(10),nullable=False)
+#     year_of_passing = db.Column(db.Integer, nullable=False)
+
+#     gender = db.Column(db.String(10),nullable=False)
+#     name_of_hospital = db.Column(db.String(100),nullable=False )
+#     Designation = db.Column(db.String(10),nullable=False)
+#     medical_council_member = db.Column(db.Integer,nullable=False)
+#     certificate = db.Column(db.String(255),nullable=False)
+#     speciality = db.Column(db.String(10),nullable=False)
+#     About_the_doctor = db.Column(db.String(255))
+
+#     # Define a relationship to the Doctor Table
+#     doctor = db.relationship('Doctor',backref= db.backref('doctor_details',lazy=True))
 
 
 @app.route('/doctor', methods = ['POST'])
@@ -73,6 +141,8 @@ def create_doctor_details():
         return jsonify({'error': 'Year_of_passing is required'}), 400
     elif 'gender' not in data:
         return jsonify({'error': 'Gender is required'}), 400
+    elif 'name_of_hospital' not in data:
+        return jsonify({'error':'Name of hospital is required'})
     elif 'Designation' not in data:
         return jsonify({'error': 'Designation  is required'}), 400
     elif 'medical_council_member' not in data:
@@ -85,6 +155,7 @@ def create_doctor_details():
 
     first_name = data['first_name']
     last_name = data['last_name']
+    name_of_hospital = data['name_of_hospital']
     phone_number = str(data['phone_number'])[-5:]  # Extract last 3 digits of phone number
     if not re.match(r'^\d+$', str(data['phone_number'])):
          return jsonify({'error': 'Invalid phone number format. Must contain 10 numeric digits.'}), 400
@@ -94,13 +165,14 @@ def create_doctor_details():
          return jsonify({'error': 'Invalid phone number length. Entered number is less than 10 digits.'}), 400
    
     user_id = f"{first_name[:2]}{last_name[:2]}{phone_number}"
-
+    #QR_GENERATOR(user_id)
+    QR_GENERATOR(user_id,first_name+' '+last_name,name_of_hospital)
     #Convert date_of_birth string to Python date
     # date_of_birth = datetime.datetime.strptime(data.get('date_of_birth', ''), '%Y-%m-%d').date() if 'date_of_birth' in data else None
     date_of_birth = datetime.strptime(data['date_of_birth'], '%Y-%m-%d').date()
     age= calculate_age(date_of_birth)
 
-    new_doctor_details = DoctorDetails(
+    new_doctor_details = Doctor(
         user_id=user_id,
         phone_number=data['phone_number'],
         first_name=data['first_name'],
@@ -110,6 +182,7 @@ def create_doctor_details():
         First_qualification = data['First_qualification'],
         year_of_passing = data['year_of_passing'],
         gender=data['gender'],
+        name_of_hospital = data['name_of_hospital'],
         Designation = data['Designation'],
         medical_council_member = data['medical_council_member'],
         certificate = data['certificate'],
@@ -128,27 +201,20 @@ def create_doctor_details():
 
 @app.route('/doctor',methods=['GET'])
 def get_all_doctor_details():
-    doctor_details = DoctorDetails.query.all()
+    doctor_details = Doctor.query.all()
     result = [{'id': detail.id, 'doctor_id': detail.user_id, 'phone_number': detail.phone_number, 'first_name': detail.first_name, 'last_name': detail.last_name,'age':detail.age,'gender':detail.gender,'speciality':detail.speciality} for detail in doctor_details]
     return jsonify(result), 200
 @app.route('/doctor/<int:doctor_detail_id>',methods=['GET'])
 def get_doctor_detail(doctor_detail_id):
-    doctor_detail = DoctorDetails.query.get_or_404(doctor_detail_id)
+    doctor_detail = Doctor.query.get_or_404(doctor_detail_id)
     return jsonify({'id': doctor_detail.id, 'user_id': doctor_detail.user_id, 'phone_number': doctor_detail.phone_number, 'first_name': doctor_detail.first_name, 'last_name': doctor_detail.last_name,'age':doctor_detail.age,'gender':doctor_detail.gender,'speciality':doctor_detail.speciality}), 200
 @app.route('/doctor/<int:doctor_detail_id>',methods=['PUT'])
 def update_doctor_detail(doctor_detail_id):
-     doctor_detail = DoctorDetails.query.get_or_404(doctor_detail_id)
+     doctor_detail = Doctor.query.get_or_404(doctor_detail_id)
      data = request.json
-     if not re.match(r'^\d+$', str(data['phone_number'])):
-         return jsonify({'error': 'Invalid phone number format. Must contain 10 numeric digits.'}), 400
-     elif len(str(data['phone_number'])) > 10:
-         return jsonify({'error': 'Invalid phone number length. Entered number is greater than 10 digits.'}), 400
-     elif len(str(data['phone_number'])) < 10:
-         return jsonify({'error': 'Invalid phone number length. Entered number is less than 10 digits.'}), 400
      doctor_detail.phone_number=data.get('phone_number', doctor_detail.phone_number)
      doctor_detail.first_name=data.get('first_name', doctor_detail.first_name)
      doctor_detail.last_name=data.get('last_name', doctor_detail.last_name)
-
      date_of_birth_str = data.get('date_of_birth')  # Assuming 'date_of_birth' is in "YYYY-MM-DD" format
      if date_of_birth_str:
     # Convert date string to Python date object
@@ -157,7 +223,6 @@ def update_doctor_detail(doctor_detail_id):
        date_of_birth_obj = doctor_detail.date_of_birth  # Keep the existing date if not provided in the data
 
      doctor_detail.date_of_birth = date_of_birth_obj
-     #doctor_detail.date_of_birth=data.get('date_of_birth', doctor_detail.date_of_birth)
      doctor_detail.First_qualification = data.get('First_qualification',doctor_detail.First_qualification)
      doctor_detail.year_of_passing = data.get('year_of_passing',doctor_detail.year_of_passing)
      doctor_detail.gender=data.get('gender', doctor_detail.gender)
@@ -166,17 +231,11 @@ def update_doctor_detail(doctor_detail_id):
      doctor_detail.certificate = data.get('certificate',doctor_detail.certificate)
      doctor_detail.speciality = data.get('speciality',doctor_detail.speciality)
      doctor_detail.About_the_doctor = data.get('About_the_doctor',doctor_detail.About_the_doctor)
-     try:
-         db.session.commit()
-         return jsonify({'message': 'Doctor detail updated successfully'}), 200
-     except IntegrityError:
-         db.session.rollback()
-         return jsonify({'error': 'Phone number already exists'}), 400
-
-
+     db.session.commit()
+     return jsonify({'message': 'Doctor detail updated successfully'}), 200
 @app.route('/doctor/<int:doctor_detail_id>',methods =['DELETE'])
 def delete_user_details(doctor_detail_id):
-    doctor_detail = DoctorDetails.query.get_or_404(doctor_detail_id)
+    doctor_detail = Doctor.query.get_or_404(doctor_detail_id)
     db.session.delete(doctor_detail)
     db.session.commit()
     return jsonify({'message': 'Doctor detail deleted successfully'}), 200
@@ -308,16 +367,9 @@ def get_user_detail(user_detail_id):
 def update_user_detail(user_detail_id):
     user_detail = UserDetails.query.get_or_404(user_detail_id)
     data = request.json
-    if not re.match(r'^\d+$', str(data['phone_number'])):
-         return jsonify({'error': 'Invalid phone number format. Must contain 10 numeric digits.'}), 400
-    elif len(str(data['phone_number'])) > 10:
-         return jsonify({'error': 'Invalid phone number length. Entered number is greater than 10 digits.'}), 400
-    elif len(str(data['phone_number'])) < 10:
-         return jsonify({'error': 'Invalid phone number length. Entered number is less than 10 digits.'}), 400
     user_detail.phone_number = data.get('phone_number', user_detail.phone_number)
     user_detail.first_name = data.get('first_name', user_detail.first_name)
     user_detail.last_name = data.get('last_name', user_detail.last_name)
-    #date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
     date_of_birth_str = data.get('date_of_birth')  # Assuming 'date_of_birth' is in "YYYY-MM-DD" format
 
     if date_of_birth_str:
@@ -327,7 +379,6 @@ def update_user_detail(user_detail_id):
        date_of_birth_obj = user_detail.date_of_birth  # Keep the existing date if not provided in the data
 
     user_detail.date_of_birth = date_of_birth_obj
-    #user_detail.date_of_birth = data.get('date_of_birth', user_detail.date_of_birth)
     user_detail.age = data.get('age', user_detail.age)
     user_detail.gender = data.get('gender', user_detail.gender)
     user_detail.marital_status = data.get('marital_status', user_detail.marital_status)
@@ -349,12 +400,8 @@ def update_user_detail(user_detail_id):
     user_detail.c_state = data.get('c_state', user_detail.c_state)
     user_detail.c_city = data.get('c_city', user_detail.c_city)
     user_detail.c_district = data.get('c_district', user_detail.c_district)
-    try:
-     db.session.commit()
-     return jsonify({'message': 'User detail updated successfully'}), 200
-    except IntegrityError:
-        db.session.rollback()
-        return jsonify({'error': 'Phone number already exists'}), 400
+    db.session.commit()
+    return jsonify({'message': 'User detail updated successfully'}), 200
 
 @app.route('/patient/<int:user_detail_id>', methods=['DELETE'])
 def delete_user_detail(user_detail_id):
@@ -406,4 +453,4 @@ def get_doctor_patients(doctor_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0'.port='8081', debug=True)
+    app.run(host='0.0.0.0',port='8081', debug=True)
